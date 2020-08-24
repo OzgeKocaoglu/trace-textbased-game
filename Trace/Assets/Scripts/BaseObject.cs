c1b8407a1360d2abd02f27dfb4c63cc27b87f16a
Özge Kocaoğlu <38084944+OzgeKocaoglu@users.noreply.github.com>
2020-08-24 06:41:52 +0300

    Base Project.
    Created game manager.
    Created first arch.

---
+++ Trace/Assets/Scripts/BaseObject.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseObject : MonoBehaviour, IUpdatable, IDestorable, IStartable
+{
+
+    public void onUpdateBaseObject()
+    {
+        Debug.Log("base object update");
+    }
+
+    public void onFixedUpdateBaseObject()
+    {
+        Debug.Log("fixed update object");
+    }
+
+    public void onDestoryBaseObject()
+    {
+      
+    }
+
+    public void onStartBaseObject()
+    {
+        Debug.Log("base object start");
+    }
+
+    public void onAwakeBaseObject()
+    {
+        
+    }
+}
---
+++ Trace/Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : BaseObject
+{
+    [Header("Objects")]
+    [SerializeField]
+    private BaseObject[] baseObjects;
+
+    private void Start()
+    {
+        CallBaseObjectStarts();
+    }
+
+
+    private void CallBaseObjectStarts()
+    {
+        for(int i = 0; i< baseObjects.Length; i++)
+        {
+            baseObjects[i].onStartBaseObject();
+        }
+    }
+
+}
